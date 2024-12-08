3818aacf22dbb45028bbdf2e783148fbc558842b
Avsh01 <avantikakumari1995@gmail.com>
2024-12-08 08:40:27 +0530

    Initial commit

---
+++ src/Java_ex/Lab001.java
@@ -0,0 +1,12 @@
+package Java_ex;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+
+
+    }
+}
